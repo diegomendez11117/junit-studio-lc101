2f5b340d6a64164e47400e61e948fcc3b7d8ca64
Enrique <diegomendez11117@gmail.com>
2021-05-06 09:50:06 -0700

    ready to upload

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,80 @@
 package test;
 
+
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+
+
+    //TODO: Check if hasBalancedBrackets returns true after passing "[]"
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+
+    //TODO: Check if hasBalancedBrackets returns true after passing ""
+    @Test
+    public void emptyValueReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns true after passing "[LaunchCode]"
+    @Test
+    public void correctValueReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns true after passing "]["
+    @Test
+    public void reverseBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns true after passing "]["
+    @Test
+    public void reverseBracketsValueReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns false after passing "]"
+    @Test
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns true after passing non null value
+    @Test
+    public void nonNullValueReturnsTrue() {
+        assertNotNull(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns true after passing "[]"
+    @Test
+    public void comparingBracketsReturnsTrue() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns true after passing "]LaunchCode["
+    @Test
+    public void comparingValueBracketsReturnsTrue() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns false after passing "]]]"
+    @Test
+    public void threeBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]"));
+    }
+
+    //TODO: Check if hasBalancedBrackets returns false after passing "[[[]]]]"
+    @Test
+    public void notSortBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]]]]"));
+    }
 
 }
